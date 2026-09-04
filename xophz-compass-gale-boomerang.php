bb96506c97971783ad375a515121172fb7483b18
XP <x@hallofthegods.com>
2026-09-03 19:55:27 -0700

    refactor: extend Xophz_Compass_Plugin_Base and remove redundant loader, i18n, and deactivator classes.

--- xophz-compass-gale-boomerang.php
+++ xophz-compass-gale-boomerang.php
@@ -54,8 +54,7 @@ function activate_xophz_compass_gale_boomerang() {
  * This action is documented in includes/class-xophz-compass-gale-boomerang-deactivator.php
  */
 function deactivate_xophz_compass_gale_boomerang() {
-  require_once plugin_dir_path( __FILE__ ) . 'includes/class-xophz-compass-gale-boomerang-deactivator.php';
-  Xophz_Compass_Gale_Boomerang_Deactivator::deactivate();
+	Xophz_Compass_Gale_Boomerang::deactivate();
 }
 
 register_activation_hook( __FILE__, 'activate_xophz_compass_gale_boomerang' );
